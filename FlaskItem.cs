e2a7ca04d0e01f17374eb94066a27a84497e9e02
crafter100500 <60203789+crafter100500@users.noreply.github.com>
2021-02-05 18:07:08 +0900

    Add files via upload

---
+++ FlaskItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FlaskItem : MonoBehaviour
+{
+    Transform flask_transform;
+    public GameObject text;
+    Transform text_transform;
+    MeshRenderer display;
+    static int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        flask_transform = GetComponent<Transform>();
+        display = GetComponent<MeshRenderer>();
+        text_transform = text.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        score = score + 1;
+        print(score);
+        display.enabled = false;
+        Destroy(text);
+        gameObject.SetActive(false);
+        if(score == 4)
+        {
+            SceneManager.LoadScene("scene2");
+        };
+    }
+}
